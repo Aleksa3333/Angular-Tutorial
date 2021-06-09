634efed1a5c32350fe7727ab202f84047d8250ec
Aleksa Međedović <amedjedovic@smart.edu.rs>
2021-06-09 18:20:28 +0200

    Treci dan prakse; Dodavanje

--- pera/src/app/heroes/heroes.component.ts
+++ pera/src/app/heroes/heroes.component.ts
@@ -1,27 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm',
-    age: 10
+    age: 10,
+    
   };
   
-  hero2:Hero={
-    id:2,
-    name:'Milan',
-    age:12
+  selectedHero?: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
-  };
-
+ hero_list: any = []; 
   constructor() { }
-  
+  metoda(){
+    this.hero_list.push({id: this.hero.id, name: this.hero.name});
+  }
   ngOnInit() {
   }
 
---
+++ pera/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11,age:22, name: 'Dr Nice' },
+  { id: 12,age:23, name: 'Narco' },
+  { id: 13,age:24, name: 'Bombasto' },
+  { id: 14,age:25, name: 'Celeritas' },
+  { id: 15,age:26, name: 'Magneta' },
+  { id: 16,age:27, name: 'RubberMan' },
+  { id: 17,age:28, name: 'Dynama' },
+  { id: 18,age:29, name: 'Dr IQ' },
+  { id: 19,age:30, name: 'Magma' },
+  { id: 20,age:31, name: 'Tornado' }
+];
